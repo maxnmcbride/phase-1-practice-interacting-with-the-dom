78a3b5d6b786be9807e46c3f502e1556719b2672
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-14 11:14:11 -0400

    working on the submit button and list

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,32 @@
 // prevent default
-const submitButton = document.querySelector('#submit')
+const submitForm = document.querySelector('#comment-form')
 // console.log(document.querySelector('#submit'));
-submitButton.addEventListener('click', (event) => {
+submitForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    // console.log(event);
-
+    console.log(event.target.comment);
 });
+
+function commentList(comment) {
+    let p = document.createElement('p');
+    document.getElementById('#list').appendChild(p)
+    p.textContent = comment;
+    p.textContent = '${comment}';
+    // console.log(document.getElementsByTagName('h3').append(p));
+    // p.textContent = comment;
+    // p.textContent = `${comment}`
+    // console.log(document.getElementsByTagName('h3').append(p))
+    // console.log(comment)
+}
+
+
+
+// function buildTodo(todo) {
+//     let p = document.createElement("p");
+//     let btn = document.createElement("button");
+//     btn.addEventListener("click", handleDelete)
+//     btn.textContent = 'x'; 
+//     p.textContent = todo;
+//     p.textContent = `${todo} `;
+//     p.appendChild(btn);
+//     document.querySelector('#tasks').appendChild(p)
+//   }
